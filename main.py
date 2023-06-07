fb97df13b9ca179ddbcf1fb5ac575b25a4d11eac
Birkin James Diana <birkin.diana@gmail.com>
2023-06-07 06:02:09 -0400

    adds logging; looks for leading space.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import copy
+import copy, logging, time
 from llama_cpp import Llama
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S' )
+log = logging.getLogger( '__name__' )
+
 ## load model -------------------------------------------------------
-print( 'Loading model...' )
+log.debug( 'Loading model...' )
 model_pth = '../models/ggml-vicuna-13b-4bit-rev1.bin'
-print( f'model_path: {model_pth}' )
+log.debug( f'model_path: {model_pth}' )
 llm = Llama( model_path=model_pth )
-print( 'model loaded.' )
+log.debug( 'model loaded.' )
 
 ## run model --------------------------------------------------------
-print( 'Running model...' )
+log.debug( 'Running model...' )
 stream = llm( 
     'Question: Who is Ada Lovelace? Answer:',
     max_tokens=100,
@@ -47,13 +53,29 @@ stream = llm(
 # if buffer:  # print any remaining text in buffer
 #     print( buffer )
 
+# buffer = ""
+# for output in stream:
+#     completion_fragment = copy.deepcopy( output )
+#     fragment = completion_fragment['choices'][0]['text']  # type: ignore
+#     buffer += fragment  # add fragment to buffer
+#     if not buffer[-1].isalnum():  # if the last character in buffer is not alphanumeric, print buffer
+#         print(buffer, end='')
+#         buffer = ""
+# if buffer:  # print any remaining text in buffer
+#     print(buffer)
+
 buffer = ""
 for output in stream:
+    # log.debug( f'output, ``{output}``' )
     completion_fragment = copy.deepcopy( output )
     fragment = completion_fragment['choices'][0]['text']  # type: ignore
     buffer += fragment  # add fragment to buffer
-    if not buffer[-1].isalnum():  # if the last character in buffer is not alphanumeric, print buffer
-        print(buffer, end='')
-        buffer = ""
+    if len(buffer) > 0:
+        if buffer[0] == ' ':
+            log.debug( f'buffer, ``{buffer}``' )
+            print(buffer, end='')
+            buffer = ""
+            time.sleep( .5 )
 if buffer:  # print any remaining text in buffer
+    log.debug( f'remaining buffer, ``{buffer}``' )
     print(buffer)
