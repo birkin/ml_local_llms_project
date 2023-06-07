051a10e6cc7a78f966d1bab1cab1477bf143adb4
Birkin James Diana <birkin.diana@gmail.com>
2023-06-07 06:38:02 -0400

    implements streaming text.

--- main.py
+++ main.py
@@ -14,23 +14,64 @@ log.debug( f'model_path: {model_pth}' )
 llm = Llama( model_path=model_pth )
 log.debug( 'model loaded.' )
 
+## ask user for input and store it to variable ----------------------
+user_input = input( 'Your input (hit return when done): ' )
+log.debug( f'user_input, ``{user_input}``' )
+
 ## run model --------------------------------------------------------
 log.debug( 'Running model...' )
+
 stream = llm( 
-    'Question: Who is Ada Lovelace? Answer:',
+    f'Question: {user_input} Answer:',
     max_tokens=100,
     stop=["\n", "Question:", "Q:"],
     stream=True,
 )
 
-## print output -----------------------------------------------------
+# stream = llm( 
+#     'Question: Who is Ada Lovelace? Answer:',
+#     max_tokens=100,
+#     stop=["\n", "Question:", "Q:"],
+#     stream=True,
+# )
 
+## print output -----------------------------------------------------
+"""
+I really wanted the output to appear word-by-word, but print() by default includes an ending newline.
+Trying print( 'the_text', end='' ) doesn't work, because the output is internally-buffered until the newline is printed.
+But this is ok; it shows that streaming the output is possible.
+For the web, some front-end js could be used to print the output word-by-word in teletype fashion.
+"""
+buffer = ""
+for output in stream:
+    # log.debug( f'output, ``{output}``' )
+    completion_fragment = copy.deepcopy( output )
+    fragment = completion_fragment['choices'][0]['text']  # type: ignore
+    buffer += fragment  # add fragment to buffer
+    # log.debug( f'buffer before if, ``{buffer}``' )
+    if ' ' in buffer:
+        # log.debug( 'about to print segment' )
+        # log.debug( f'buffer, ``{buffer}``' )
+        # print( buffer, end='' )
+        print( buffer)
+        # print( 'foo' )
+        # log.debug( 'segment printed' )
+        buffer = ""
+        # time.sleep( .25 )
+if buffer:  # print any remaining text in buffer
+    log.debug( f'remaining buffer, ``{buffer}``' )
+    print(buffer)
+    
+log.debug( 'model run complete.' )
+
+## tutorial ---
 # for output in stream:
 #     completion_fragment = copy.deepcopy( output )
 #     # fragment = completion_fragment['choices'][0]['text']
 #     fragment = completion_fragment.get('choices', [])[0]['text']
 #     print( fragment )
 
+## trying to print word-by-word ---
 # buffer = ""
 # for output in stream:
 #     completion_fragment = copy.deepcopy( output )
@@ -41,6 +82,7 @@ stream = llm(
 #         print(buffer, end='')
 #         buffer = ""
 
+## trying to print word-by-word ---
 # buffer = ""
 # for output in stream:
 #     completion_fragment = copy.deepcopy( output )
@@ -53,6 +95,7 @@ stream = llm(
 # if buffer:  # print any remaining text in buffer
 #     print( buffer )
 
+## trying to print word-by-word ---
 # buffer = ""
 # for output in stream:
 #     completion_fragment = copy.deepcopy( output )
@@ -64,18 +107,20 @@ stream = llm(
 # if buffer:  # print any remaining text in buffer
 #     print(buffer)
 
-buffer = ""
-for output in stream:
-    # log.debug( f'output, ``{output}``' )
-    completion_fragment = copy.deepcopy( output )
-    fragment = completion_fragment['choices'][0]['text']  # type: ignore
-    buffer += fragment  # add fragment to buffer
-    if len(buffer) > 0:
-        if buffer[0] == ' ':
-            log.debug( f'buffer, ``{buffer}``' )
-            print(buffer, end='')
-            buffer = ""
-            time.sleep( .5 )
-if buffer:  # print any remaining text in buffer
-    log.debug( f'remaining buffer, ``{buffer}``' )
-    print(buffer)
+## trying to print word-by-word ---
+# buffer = ""
+# for output in stream:
+#     # log.debug( f'output, ``{output}``' )
+#     completion_fragment = copy.deepcopy( output )
+#     fragment = completion_fragment['choices'][0]['text']  # type: ignore
+#     buffer += fragment  # add fragment to buffer
+#     log.debug( f'buffer before if, ``{buffer}``' )
+#     if len(buffer) > 0:
+#         if buffer[0] == ' ':
+#             log.debug( f'buffer, ``{buffer}``' )
+#             print(buffer, end='')
+#             buffer = ""
+#             time.sleep( .5 )
+# if buffer:  # print any remaining text in buffer
+#     log.debug( f'remaining buffer, ``{buffer}``' )
+#     print(buffer)
