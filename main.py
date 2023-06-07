7e9cf02308caee24cc6c261d1b81b02e7f0462b5
Birkin James Diana <birkin.diana@gmail.com>
2023-06-06 20:23:19 -0400

    trying to get nice streamed spaced output.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import copy
 from llama_cpp import Llama
 
 ## load model -------------------------------------------------------
@@ -10,13 +10,50 @@ print( 'model loaded.' )
 
 ## run model --------------------------------------------------------
 print( 'Running model...' )
-output = llm( 
+stream = llm( 
     'Question: Who is Ada Lovelace? Answer:',
     max_tokens=100,
     stop=["\n", "Question:", "Q:"],
-    echo=True,
+    stream=True,
 )
 
 ## print output -----------------------------------------------------
-output_jsn = json.dumps( output, sort_keys=True, indent=2 )
-print( f'output_jsn, ``{output_jsn}``' )
+
+# for output in stream:
+#     completion_fragment = copy.deepcopy( output )
+#     # fragment = completion_fragment['choices'][0]['text']
+#     fragment = completion_fragment.get('choices', [])[0]['text']
+#     print( fragment )
+
+# buffer = ""
+# for output in stream:
+#     completion_fragment = copy.deepcopy( output )
+#     fragment = completion_fragment.get('choices', [])[0]['text']
+#     buffer += fragment  # add fragment to buffer
+#     if buffer and (buffer[-1] in {' ', ',', '.', '!', '?', ';', ':', '-'} or buffer[-1].isdigit()):
+#         # print buffer and clear it when a space, punctuation, or digit is detected
+#         print(buffer, end='')
+#         buffer = ""
+
+# buffer = ""
+# for output in stream:
+#     completion_fragment = copy.deepcopy( output )
+#     fragment = completion_fragment.get('choices', [])[0]['text']
+#     buffer += fragment  # add fragment to buffer
+#     # print( f'buffer is now: {buffer}, and ends with, ``{buffer[-1]}' )
+#     if buffer.endswith(' '): # if buffer ends with a space, print it and clear it
+#         print(buffer, end='')
+#         buffer = ""
+# if buffer:  # print any remaining text in buffer
+#     print( buffer )
+
+buffer = ""
+for output in stream:
+    completion_fragment = copy.deepcopy( output )
+    fragment = completion_fragment.get('choices', [])[0]['text']
+    buffer += fragment  # add fragment to buffer
+    if not buffer[-1].isalnum():  # if the last character in buffer is not alphanumeric, print buffer
+        print(buffer, end='')
+        buffer = ""
+if buffer:  # print any remaining text in buffer
+    print(buffer)
